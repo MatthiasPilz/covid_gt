f832b2f689329021fa622e3163be4363ee95c0b9
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2020-07-01 18:05:16 +0100

    reading data

--- src/game.py
+++ src/game.py
@@ -1,4 +1,6 @@
 import datetime
+import pandas as pd
+import numpy as np
 
 
 class Game:
@@ -13,9 +15,23 @@ class Game:
         self.pricing_parameter.append(params['pricing-coeff'][1])
         self.pricing_parameter.append(params['pricing-coeff'][2])
 
+        dateparse = lambda x: datetime.datetime.strptime(x, '%d/%m/%Y')
+        self.demand = pd.read_csv(params['demand-file'],
+                                  sep = ',',
+                                  skipinitialspace = True,
+                                  dtype={'east': np.int32,
+                                         'london': np.int32,
+                                         'midlands': np.int32,
+                                         'north_east': np.int32,
+                                         'north_west': np.int32,
+                                         'south_east': np.int32,
+                                         'south_west': np.int32, },
+                                  parse_dates=['date'],
+                                  date_parser=dateparse)
+
     def display(self):
         """Display Configuration values."""
-        print("\nConfigurations:")
+        print("\ngame class variables:")
         for a in dir(self):
             if not a.startswith("__") and not callable(getattr(self, a)):
                 print("{:30} {}".format(a, getattr(self, a)))
